79b623f74cdcdbc1ed3335d37aaa92d307e61880
xiao fu <7938813+xiaofu2024@users.noreply.github.com>
2024-09-12 23:28:07 +0800

    Update NetworkUtil.swift

--- TeneasyChatSDKUI_iOS/Classes/NetWork/NetworkUtil.swift
+++ TeneasyChatSDKUI_iOS/Classes/NetWork/NetworkUtil.swift
@@ -63,7 +63,7 @@ enum NetworkUtil {
                 }
             case .failure(let error):
                 print(error)
-            logError(request: "", header: String(describing: task.headers), body: String(describing: error), code: 1001, url: "\(task.baseURL)\(task.path)")
+            logError(request: "", header: String(describing: task.headers), body: String(describing: error), code: 101, url: "\(task.baseURL)\(task.path)")
                 done(false, nil)
             }
         }
@@ -90,7 +90,7 @@ enum NetworkUtil {
                     }
                 case .failure(let error):
                     print(error)
-                logError(request: "", header: String(describing: task.headers), body: String(describing: error), code: 1001, url: "\(task.baseURL)\(task.path)")
+                logError(request: "", header: String(describing: task.headers), body: String(describing: error), code: 101, url: "\(task.baseURL)\(task.path)")
                     done(false, nil)
             }
         }
@@ -115,7 +115,7 @@ enum NetworkUtil {
                 }
             case .failure(let error):
                 print(error)
-            logError(request: "", header: String(describing: task.headers), body: String(describing: error), code: 1001, url: "\(task.baseURL)\(task.path)")
+            logError(request: "", header: String(describing: task.headers), body: String(describing: error), code: 101, url: "\(task.baseURL)\(task.path)")
                 done(false, nil)
             }
         }
@@ -144,7 +144,7 @@ enum NetworkUtil {
                 }
             case .failure(let error):
                 print(error)
-            logError(request: "", header: String(describing: task.headers), body: String(describing: error), code: 1001, url: "\(task.baseURL)\(task.path)")
+            logError(request: "", header: String(describing: task.headers), body: String(describing: error), code: 101, url: "\(task.baseURL)\(task.path)")
                 done(false, nil)
             }
         }
@@ -179,13 +179,15 @@ enum NetworkUtil {
         print("请求路径: \(task.baseURL)\(task.path)===\(task.method)")
         //task.headers?.description.addingPercentEscapes(using: <#T##String.Encoding#>)
         print("请求header: \(String(describing: task.headers))")
+        print("正在上报错误\(reportRequest)")
         ChatProvider.request(task) { result in
             switch result {
                 case .success(let response):
                     print(response)
                     let dic = try? response.mapJSON() as? [String: Any]
-//                    print("=====\(dic)")
+                print("=====\(String(describing: dic))")
                     let result = BaseRequestResult<Any>.deserialize(from: dic)
+                
                     if result?.code == 0 {
                         done(true, result)
                     } else {
@@ -200,6 +202,8 @@ enum NetworkUtil {
     }
     
     static func logError(request: String, header: String, body: String, code: Int, url: String){
+        
+        print("记录错误日志")
         //无可用线路是大事件，需要上报
         let errorItem = ErrorItem()
         errorItem.code = code
@@ -207,9 +211,6 @@ enum NetworkUtil {
         
         // "platform": 1, // Platform_IOS: 1;Platform_ANDROID: 2;Platform_H5: 4;
         errorItem.platform = 1
-        //"2024-09-11T22:22:22Z"
-        //\"2024-09-09T22:57:00+0800\""
-        //"yyyy-MM-dd'T'HH:mm:ssZ"
         errorItem.createdAt = Date().toString(format: "yyyy-MM-dd'T'HH:mm:ss'Z'")
         
         let errorPayload = ErrorPayload()
@@ -218,11 +219,8 @@ enum NetworkUtil {
         errorPayload.header = header
         errorItem.payload = errorPayload.toJSONString()
         reportRequest.data.append(errorItem)
-        
-        
-        //if reportRequest.data.count == 1{
-            doReportError()
-        //}
+
+        doReportError()
     }
     
     static func doReportError(){
@@ -230,7 +228,10 @@ enum NetworkUtil {
             return
         }
         print("上报错误")
-        print(reportRequest)
+        if let jsonString = convertToJSONString(from: reportRequest) {
+            debugPrint(jsonString)
+        }
+       
         NetworkUtil.reportError(reportRequest: reportRequest){ success, data in
             if success{
                 print("上报错误成功")
@@ -240,4 +241,17 @@ enum NetworkUtil {
             }
         }
     }
+    
+    static func convertToJSONString<T: Encodable>(from model: T) -> String? {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted // 如果想要格式化输出，可以去掉这个选项
+        do {
+            let jsonData = try encoder.encode(model)
+            return String(data: jsonData, encoding: .utf8)
+        } catch {
+            print("Failed to encode model: \(error)")
+            return nil
+        }
+    }
+
 }
